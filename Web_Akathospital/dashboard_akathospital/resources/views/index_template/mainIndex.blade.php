bc7ea8a4e50f67c87d220f1c55a20392e0ba4ffb
taclublove <chanathipbadboy@gmail.com>
2023-08-02 16:05:40 +0700

    change

--- Web_Akathospital/dashboard_akathospital/resources/views/index_template/mainIndex.blade.php
+++ Web_Akathospital/dashboard_akathospital/resources/views/index_template/mainIndex.blade.php
@@ -8,6 +8,7 @@
 </head>
 
 <body>
+    {{-- Login Dashboard Start --}}
     <div class="modal fade " id="manageWebPage" tabindex="-1" aria-labelledby="exampleModalLabel"
         data-bs-backdrop="static" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
@@ -16,7 +17,7 @@
                     <h5 class="modal-title" id="exampleModalLabel">Login</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <form action="{{ route('login') }}" method="POST" >
+                <form action="" method="POST" >
                     @csrf
                     <div class="modal-body p-4 bg-light">
                         <div class="row">
@@ -40,6 +41,7 @@
             </div>
         </div>
     </div>
+    {{-- Login Dashboard End --}}
 
     @include('index_template.patials.navbar')
 
