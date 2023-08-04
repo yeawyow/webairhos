eff9d8734728412ec0fadb16c78d4421f5f423ff
talublove <chanathipbadboy@gmail.com>
2023-08-04 16:46:09 +0700

    Change

--- Web_Akathospital/dashboard_akathospital/resources/views/index.blade.php
+++ Web_Akathospital/dashboard_akathospital/resources/views/index.blade.php
@@ -99,7 +99,7 @@
 <div class="container mb-5">
     <div class="row">
         <div class="col-4">
-            <div class="accordion" style="width: 400px;" id="accordionExample" data-aos="fade-up" data-aos-duration="1500">
+            <div class="accordion rounded-2" style="width: 400px; box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.5);" id="accordionExample" data-aos="fade-up" data-aos-duration="1500">
                 <div class="accordion-item">
                     <h2 class="accordion-header" id="headingOne">
                         <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
@@ -110,37 +110,37 @@
                         <div class="accordion-body">
                             <ul>
                                 <li style="list-style: none;" class="mt-2">
-                                    <a class="btn btn-warning" style="text-decoration: none; width: 280px;" href="http://192.168.2.7/backoffice" target="_blank">BackOffice</a>
+                                    <a class="btn btn-outline-success" style="text-decoration: none; width: 280px;" href="http://192.168.2.7/backoffice" target="_blank">BackOffice</a>
                                 </li>
                                 <li style="list-style: none;" class="mt-2"> 
-                                    <a class="btn btn-warning" style="text-decoration: none; width: 280px;" href="http://192.168.2.3:8082/#/login" target="_blank">SMART REFER</a>
+                                    <a class="btn btn-outline-success" style="text-decoration: none; width: 280px;" href="http://192.168.2.3:8082/#/login" target="_blank">SMART REFER</a>
                                 </li>
                                 <li style="list-style: none;" class="mt-2">
-                                    <a class="btn btn-warning" style="text-decoration: none; width: 280px;" href="http://ae.moph.go.th/isonline/" target="_blank">IS ONLINE</a>
+                                    <a class="btn btn-outline-success" style="text-decoration: none; width: 280px;" href="http://ae.moph.go.th/isonline/" target="_blank">IS ONLINE</a>
                                 </li>
                                 <li style="list-style: none;" class="mt-2">
-                                    <a class="btn btn-warning" style="text-decoration: none; width: 280px;" href="https://www.kaotajai.com/login" target="_blank">ก้าวท้าใจ</a>
+                                    <a class="btn btn-outline-success" style="text-decoration: none; width: 280px;" href="https://www.kaotajai.com/login" target="_blank">ก้าวท้าใจ</a>
                                 </li>
                                 <li style="list-style: none;" class="mt-2">
-                                    <a class="btn btn-warning" style="text-decoration: none; width: 280px;" href="http://203.157.177.6/archives/" target="_blank">ระบบสารบัญ'สาสุข</a>
+                                    <a class="btn btn-outline-success" style="text-decoration: none; width: 280px;" href="http://203.157.177.6/archives/" target="_blank">ระบบสารบัญ'สาสุข</a>
                                 </li>
                                 <li style="list-style: none;" class="mt-2">
-                                    <a class="btn btn-warning" style="text-decoration: none; width: 280px;" href="https://snk.hdc.moph.go.th/hdc/main/index_pk.php" target="_blank">HDC43แฟ้ม(จังหวัด)</a>
+                                    <a class="btn btn-outline-success" style="text-decoration: none; width: 280px;" href="https://snk.hdc.moph.go.th/hdc/main/index_pk.php" target="_blank">HDC43แฟ้ม(จังหวัด)</a>
                                 </li>
                                 <li style="list-style: none;" class="mt-2">
-                                    <a class="btn btn-warning" style="text-decoration: none; width: 280px;" href="https://skko.moph.go.th/iclaimer/main/index.php" target="_blank">โปรแกรม iclamer</a>
+                                    <a class="btn btn-outline-success" style="text-decoration: none; width: 280px;" href="https://skko.moph.go.th/iclaimer/main/index.php" target="_blank">โปรแกรม iclamer</a>
                                 </li>
                                 <li style="list-style: none;" class="mt-2">
-                                    <a class="btn btn-warning" style="text-decoration: none; width: 280px;" href="https://r8way.moph.go.th/r8way/" target="_blank">เขตสุขภาพที่ 8</a>
+                                    <a class="btn btn-outline-success" style="text-decoration: none; width: 280px;" href="https://r8way.moph.go.th/r8way/" target="_blank">เขตสุขภาพที่ 8</a>
                                 </li>
                                 <li style="list-style: none;" class="mt-2">
-                                    <a class="btn btn-warning" style="text-decoration: none; width: 280px;" href="https://www.cgd.go.th/cs/internet/internet/%e0%b8%ab%e0%b8%99%e0%b9%89%e0%b8%b2%e0%b8%ab%e0%b8%a5%e0%b8%b1%e0%b8%81.html" target="_blank">กรมบัญชีกลาง</a>
+                                    <a class="btn btn-outline-success" style="text-decoration: none; width: 280px;" href="https://www.cgd.go.th/cs/internet/internet/%e0%b8%ab%e0%b8%99%e0%b9%89%e0%b8%b2%e0%b8%ab%e0%b8%a5%e0%b8%b1%e0%b8%81.html" target="_blank">กรมบัญชีกลาง</a>
                                 </li>
                                 <li style="list-style: none;" class="mt-2">
-                                    <a class="btn btn-warning" style="text-decoration: none; width: 280px;" href="http://www.gprocurement.go.th/new_index.html" target="_blank">ระบบจัดซื้อจัดจ้างภาครัฐ</a>
+                                    <a class="btn btn-outline-success" style="text-decoration: none; width: 280px;" href="http://www.gprocurement.go.th/new_index.html" target="_blank">ระบบจัดซื้อจัดจ้างภาครัฐ</a>
                                 </li>
                                 <li style="list-style: none;" class="mt-2">
-                                    <a class="btn btn-warning" style="text-decoration: none; width: 280px;" href="https://coop-online.phsncoop.com/" target="_blank">สหกรณ์ออมทรัพย์สาธารณสุขสกลนคร</a>
+                                    <a class="btn btn-outline-success" style="text-decoration: none; width: 280px;" href="https://coop-online.phsncoop.com/" target="_blank">สหกรณ์ออมทรัพย์สาธารณสุขสกลนคร</a>
                                 </li>
                             </ul>
                         </div>
