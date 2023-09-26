51ac40c2a6c06c10173239b0e25149b7e79bd5c0
taclublove <chanathipbadboy@gmail.com>
2023-09-26 10:48:07 +0700

    Change

--- Akathospital/resources/views/adminDashboard.blade.php
+++ Akathospital/resources/views/adminDashboard.blade.php
@@ -2,12 +2,35 @@
 
     @section('content')
 
-        <main class="content px-3 py-2 mt-3 " >
-            <div class="container-fluid">
-                <div class="mb-3 mt-2" style="margin-left: 15rem;">
-                    <h3>Admin Dashboard</h3>
+    <div id="content" class="p-4 p-md-5 pt-5" style="margin-left: 15rem;">
+            <h2 class="mb-4 ms-4">Sidebar #05</h2>
+            <div class="card p-3 rounded rounded-5">
+                <div class="container">
+                    <table id="myTable" class="table table-striped" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Image</th>
+                                <th>Edit & Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($sdjs as $sd)
+                            <tr>
+                                <td>{{ $sd->sdjs_id }}</td>
+                                <td>{{ $sd->sdjs_name }}</td>
+                                <td><img src="storage/images/sliderJssor/{{ $sd->sdjs_image }}" alt="" width="50" class="img-thumbnail rounded"></td>
+                                <td>
+                                    <a href="#" id="' . $sj->sdjs_id . '" class="btn btn-outline-success mx-1 editIcon" data-bs-toggle="modal" data-bs-target="#sliderJssorModal"><i class="bi-pencil-square h4">Edit</i></a>
+                                    <a href="#" id="' . $sj->sdjs_id . '" class="btn btn-outline-danger mx-1 deleteIcon"><i class="bi-trash h4">Delete</i></a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
-        </main>
+        </div>
 
     @endsection
--- index.php
+++ index.php
@@ -1,57 +1,19 @@
-<?php
-
-    $servername = "202.80.228.46";
-    $username = "sa";
-    $password = "sa";
-
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=airoffice", $username, $password);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        // echo "Connected successfully";
-    } catch(PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-    }
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test</title>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" />
+    <!-- Bootstrap 5 Start -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <!-- Bootstrap 5 End -->
+    
+    <title>Admin Dashboard</title>
 </head>
 <body>
-<table id="myTable" class="display" style="width: 100%;">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php 
-                $stmt = $conn->prepare("SELECT * FROM airoffice");
-                $stmt->execute();
-            ?>
-            <?php foreach($result as $re) {  ?>
-            <tr>
-                <td><?php echo $re['hn']; ?></td>
-                <td><?php echo $re['pname']; ?></td>
-            </tr>
-            <?php } ?>
-        </tbody>
-    </table>
-    <script>
-        $(document).ready(function () {
-            $("#myTable").DataTable();
-        });
-    </script>
+    
+    
+    
 </body>
 </html>
